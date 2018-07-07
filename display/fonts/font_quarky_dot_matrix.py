fc1d3b389405f74d657d507dfcccbcb349c91c60
Michael Chan <mickchan@gmail.com>
2018-07-07 18:05:46 +1000

    Added Quarky font to print_char.py

--- display/fonts/font_quarky_dot_matrix.py
+++ display/fonts/font_quarky_dot_matrix.py
@@ -2,6 +2,23 @@
 # * DotMatrix (https://www.urbanfonts.com/fonts/DotMatrix.font) for lowercase
 # * Quarky (https://cooltext.com/Fonts-Dot-Matrix) for uppercase
 
+from font import Font
+
+class FontQuarkyDotMatrix(Font):
+
+    def __init__(self):
+        Font.__init__(self)
+
+    def getWidth(self):
+        return 4
+
+    def getHeight(self):
+        return 8
+
+    def getPixel(self, x, y):
+        return (font_bitmap[self.mChar][x] & (1 << (y)))
+
+
 font_bitmap = {
 " " : [
     #    32 ' '
@@ -384,4 +401,3 @@ font_bitmap = {
 
 ]
 }
-
--- display/fonts/print_char.py
+++ display/fonts/print_char.py
@@ -1,13 +1,34 @@
+import sys
 from font_atari_4x8 import FontAtariSmall
+from font_quarky_dot_matrix import FontQuarkyDotMatrix
+def createFont(fontName):
+    print "Create font:", fontName
+    if fontName == "atari":
+        print "Created atari font"
+        return FontAtariSmall()
+    elif fontName == "quarky":
+        print "Created quarky font"
+        return FontQuarkyDotMatrix()
+    else:
+        return None
 
-f = FontAtariSmall()
-f.setCharacter("A")
-for y in range(f.getHeight()):
-    line = "|"
-    for x in range(f.getWidth()):
-        if f.getPixel(x, y) != 0:
-            line += "#"
-        else:
-            line += " "
-    line += "|"
-    print line
+def main():
+    print "Starting"
+    if len(sys.argv) != 3:
+        print "Usage: %s <font name> <character>" % (sys.argv[0])
+        return
+    f = createFont(sys.argv[1])
+    f.setCharacter(sys.argv[2][0])
+    for y in range(f.getHeight()):
+        line = "|"
+        for x in range(f.getWidth()):
+            if f.getPixel(x, y) != 0:
+                line += "##"
+            else:
+                line += "  "
+        line += "|"
+        print line
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
